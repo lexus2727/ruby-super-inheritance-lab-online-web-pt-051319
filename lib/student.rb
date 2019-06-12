8c0f01b19b6b18119ca3699346d330721a4be6ff
Candice Parker <lexus2424@gmail.com>
2019-06-12 13:49:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,12 @@ class Student
   
   def hello
     
-    puts "Hey there! I'm so excited to learn stuff"
+    puts "Hey there! I'm so excited to learn stuff."
+  end
+  
+  def raise_hand
+    
+    put "Pick me!"
   end
   
 end
